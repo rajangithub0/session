b1e85707aea088f5668c4cd4477b2f093acaabd9
rajangithub0 <122583123+rajangithub0@users.noreply.github.com>
2023-03-27 22:32:47 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+// excrise 3
+function addTwo(num1, num2) {
+  let sum = num1 + num2;
+  console.log("addiition", sum);
+}
+addTwo(34, 34);
+
+// excrise 2
+
+function makemala(flower, color) {
+  console.log("bring the suii", flower, color);
+  console.log("put dagha into suii", flower, color);
+  console.log("arrange flower one by one", flower, color);
+}
+makemala("rose", "red");
+makemala("lily", "white");
+makemala("lotus", "pink");
+makemala("jasmine", "yelllow");
+//excrise 1
+function Sabji(allu) {
+  console.log("wash patato", allu);
+  console.log("put on flame", allu);
+  console.log("cut the patato");
+}
+Sabji("salt");
